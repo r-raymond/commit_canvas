83499a37aae6d5c214539c5d121d051a67b723bd
Robin Raymond <robin.raymond@taktile.com>
2024-11-25 08:04:51 +0100

    chore(debug): Rename debugging feature

--- infrastructure_browser/src/utils.rs
+++ infrastructure_browser/src/utils.rs
@@ -4,13 +4,13 @@ use std::fmt;
 use wasm_bindgen::JsValue;
 
 pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // When the `debug` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
     // we will get better error messages if our code ever panics.
     //
     // For more details see
     // https://github.com/rustwasm/console_error_panic_hook#readme
-    #[cfg(feature = "console_error_panic_hook")]
+    #[cfg(feature = "debug")]
     console_error_panic_hook::set_once();
 }
 
