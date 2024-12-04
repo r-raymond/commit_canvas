f1ae6d0d5c0203b43e807d9bbead038869ca9f13
Robin Raymond <robin.raymond@taktile.com>
2024-12-04 08:02:06 +0100

    fix(clippy): Const thread local

--- infrastructure_browser/src/globals.rs
+++ infrastructure_browser/src/globals.rs
@@ -18,5 +18,5 @@ thread_local! {
     pub static SVG_VIEW_GROUP: web_sys::SvgElement = DOCUMENT.with(|d| d.get_element_by_id("cc_group_view").expect("No svg found").dyn_into::<web_sys::SvgElement>().expect("Failed to cast to SvgElement"));
     pub static SVG_CONTROL_GROUP: web_sys::SvgElement = DOCUMENT.with(|d| d.get_element_by_id("cc_group_control").expect("No svg found").dyn_into::<web_sys::SvgElement>().expect("Failed to cast to SvgElement"));
     pub static CONTROL: RefCell<Control<Marker, Selection>> = RefCell::new(Control::new(Box::new(update)));
-    pub static PAN_AND_ZOOM_STATE: RefCell<PanAndZoom> = RefCell::new(PanAndZoom { pan: (0.0, 0.0), zoom: 1.0, size: (0.0, 0.0) });
+    pub static PAN_AND_ZOOM_STATE: RefCell<PanAndZoom> = const { RefCell::new(PanAndZoom { pan: (0.0, 0.0), zoom: 1.0, size: (0.0, 0.0) }) };
 }
